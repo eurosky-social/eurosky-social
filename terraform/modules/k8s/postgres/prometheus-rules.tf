1aa169bd139f3c5d85fc2618da07902c4f34345c
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2025-11-11 17:33:50 +0000

    fix: update LastFailedArchiveTime alert to exclude replicas in Prometheus rules

--- terraform/modules/k8s/postgres/prometheus-rules.tf
+++ terraform/modules/k8s/postgres/prometheus-rules.tf
@@ -10,18 +10,39 @@ data "http" "cnpg_alerts" {
   }
 }
 
-resource "kubectl_manifest" "cnpg_alerts" {
-  yaml_body = yamldecode(data.http.cnpg_alerts.response_body) != null ? (
-    yamlencode(merge(
-      yamldecode(data.http.cnpg_alerts.response_body),
-      {
-        metadata = merge(
-          yamldecode(data.http.cnpg_alerts.response_body).metadata,
-          { namespace = "databases" }
-        )
+locals {
+  cnpg_alerts = yamldecode(data.http.cnpg_alerts.response_body)
+
+  # Fix LastFailedArchiveTime alert to exclude replicas
+  # See: https://github.com/cloudnative-pg/cloudnative-pg/issues/7086
+  fixed_rules = [
+    for rule in local.cnpg_alerts.spec.groups[0].rules :
+    rule.alert == "LastFailedArchiveTime" ? merge(rule, {
+      expr = "(1 - cnpg_pg_replication_in_recovery) * (cnpg_pg_stat_archiver_last_failed_time - cnpg_pg_stat_archiver_last_archived_time) > 1"
+    }) : rule
+  ]
+
+  cnpg_alerts_fixed = merge(
+    local.cnpg_alerts,
+    {
+      metadata = merge(
+        local.cnpg_alerts.metadata,
+        { namespace = "databases" }
+      )
+      spec = {
+        groups = [
+          merge(
+            local.cnpg_alerts.spec.groups[0],
+            { rules = local.fixed_rules }
+          )
+        ]
       }
-    ))
-  ) : data.http.cnpg_alerts.response_body
+    }
+  )
+}
+
+resource "kubectl_manifest" "cnpg_alerts" {
+  yaml_body = yamlencode(local.cnpg_alerts_fixed)
 
   depends_on = [kubernetes_namespace.databases]
 }
